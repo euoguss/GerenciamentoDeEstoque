f87ff38bd0f1b61c5587deda7893fba5b5321499
euoguss <dias.gustavof@gmail.com>
2024-11-02 21:12:39 -0300

    integração inicial do front

---
+++ frontend/src/App.js
@@ -0,0 +1,44 @@
+import GlobalStyle from "./styles/global";
+import styled from "styled-components";
+import Form from "./components/Form.js";
+import Grid from "./components/Grid.js";
+import { useEffect, useState } from "react";
+import {ToastContainer, toast} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import axios from "axios";
+
+const Container = styled.div` width: 100%; max-width: 800px; margin-top: 20px; display: flex; flex-direction: column; align-items: center; gap: 10px; `;
+
+const Title = styled.h2``;
+function App() {
+  const [users, setUsers] = useState([]);
+  const [onEdit, setonEdit] = useState(null);
+
+  const getUsers = async () => {
+    try{
+      const res = await axios.get("http://localhost:8800");
+      setUsers(res.data.sort((a, b) => (a.nome > b.nome ? 1 : -1)));
+    } catch (error) {
+      toast.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, [setUsers]);
+
+   return (
+     <> 
+     <Container> 
+      <Title>
+        USUÁRIOS
+        </Title> 
+      <Form/> 
+      <Grid users={users}/>
+      </Container> 
+      <ToastContainer autoClose={3000} position={"bottom-right"}/> 
+      <GlobalStyle/> </> 
+      ); 
+    }; 
+      
+  export default App;
